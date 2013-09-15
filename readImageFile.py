71f5d789a3c1983e892206748ff33bd80883b952
Omar Jatoi <ouj@andrew.cmu.edu>
2013-09-14 23:50:14 -0400

    Added function to add the binary data to the image.

--- readImageFile.py
+++ readImageFile.py
@@ -1,4 +1,5 @@
 import Image
+from readTargetFile.py import *
 
 def initializeImage():
 	imageName = raw_input("Please enter image file name: ")
@@ -36,11 +37,60 @@ def clearLowestBits(RGBtupleList):
 		clearedList.append(tempTuple)
 	return clearedList
 
-def putDataInImage(RGBlist, fileList):
+def putDataInImage(RGBlist, fileList, imageList):
+	modifiedImageTupleList = []
 	
+	imageSize = imageList[1] #this is a tuple
+	2bitChunkListLength = len(fileList[2])
+	formatBinary = fileList[0]		
+	lengthBinary = fileList[1]
+	dataBinary = fileList[2]
+	
+	if (len(formatBinary) + len(lengthBinary) + 2bitChunkListLength) > 6*len(RGBlist):
+		print "Too much data. Try larger image."
 
+	pixelCounter = 0
+	
+	index = 0
+	while index < len(formatBinary):
+		tempTuple = (0, 0, 0)
+		for j in range(3):
+			temp = RGBlist[pixelCounter][j] | "{0:0>8}".format(formatBinary[index])
+			tempTuple[j] = temp
+			index += 1
+		modifiedImageTupleList.append(tempTuple)			
+		pixelCounter += 1
+	
+	formatEndLocation = pixelCounter	
+
+	index2 = 0
+	while index2 < len(lengthBinary):
+		tempTuple = (0, 0, 0)
+		for j in range(3):
+			temp = RGBlist[pixelCounter][j] | "{0:0>8}".format(lengthBinary[index2])
+			tempTuple[j] = temp
+			index2 += 1
+		modifiedImageTupleList.append(tempTuple)
+		pixelCounter += 1
+
+	lengthEndLocation = pixelCounter
+
+	index3 = 0
+	while index3 < 2bitChunkListLength:
+		tempTuple = (0, 0, 0)
+		for j in range(3):
+			temp = RGBlist[pixelCounter][j] | "{0:0>8}".format(dataBinary[index3])
+			tempTuple[j] = temp
+			index3 += 1
+		modifiedImageTupleList.append(tempTuple)
+		pixelCounter += 1
+
+	dataEndLocation = pixelCounter
+	
 def createModifiedImage(tupleList, imageSize):
 	modifiedImage = Image.new(RGB, imageSize)
 	modifiedImage.putdata(tupleList)
 	modifiedImage.save("ModifiedImage.jpg")
+	print "New image with hidden encrypted data saved as 'ModifiedImage.jpg'"
+	
 	
--- readTargetFile.py
+++ readTargetFile.py
@@ -21,22 +21,30 @@ fileList = []
 def stringToBin(string):
 	return bin(int(binascii.hexlify(string), 16))
 
-def BinToString(binaryString):
-	return binascii.unhexlify('%x' % binaryString)
+def binToString(binaryString):
+	return binascii.unhexlify('%x' % int(binaryString, 2))
 
 def constructFileList(fileName):
 	
 	binaryEncryptedData = stringToBin(encryptedData)
-	binaryFileFormat = stringToBin(fileFormat)
-	
+	binaryFileFormat = stringToBin(fileFormat)	
 	dataLength = len(binaryEncryptedData)
+
+	splitFormat = []
+	for x in range(0, len(binaryFileFormat), 2):
+		splitFormat.append(binaryFileFormat[x:x+2]
 	
-	splitList = []
+	splitLength = []
+	for x in range(0, len(binaryEncryptedData), 2):
+		splitLength.append(bin(dataLength)[2:][x:x+2])
+
+	splitDataList = []
 	for x in range(0, dataLength, 2):
-		splitList.append(binaryEncryptedData[x:x+2])
+		splitDataList.append(binaryEncryptedData[x:x+2])
 	
-	fileList.append(fileFormat)
-	fileList.append(dataLength)
-	fileList.append(splitList) # fileList[2] gives data (string)
+	fileList.append(splitFormat)
+	fileList.append(splitLength)
+	fileList.append(splitDataList) # fileList[2] gives data (string)
 	
 	return fileList
+
